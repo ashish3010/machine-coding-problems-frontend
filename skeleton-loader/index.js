83a2816abc8ab83e689ebb050c4aeb55bcbe1ed0
Ashish Mishra <69194675+ashish3010@users.noreply.github.com>
2023-11-02 16:37:51 +0530

    Create index.js

---
+++ skeleton-loader/index.js
@@ -0,0 +1,16 @@
+import Skeleton from "./skeleton";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Skeleton width="200px" height="200px" radius="100%" />
+      <div style={{ height: "10px" }} />
+      <Skeleton width="50%" height="50px" radius="24px" />
+      <div style={{ height: "10px" }} />
+      <Skeleton width="80%" height="100px" radius="24px" />
+      <div style={{ height: "10px" }} />
+      <Skeleton width="100%" height="300px" radius="24px" />
+    </div>
+  );
+}
