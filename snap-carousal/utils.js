7dec439e6f695c3aa06c26d31a4ae1fba9f37062
Ashish Mishra <69194675+ashish3010@users.noreply.github.com>
2025-04-12 23:49:18 +0530

    Create utils.js

---
+++ snap-carousal/utils.js
@@ -0,0 +1,32 @@
+export const data = [
+  {
+    id: "1",
+    title: "Image 1",
+    image:
+      "https://wallpaperswide.com/download/macos_sierra_2-wallpaper-3554x1999.jpg",
+  },
+  {
+    id: "2",
+    title: "Image 2",
+    image:
+      "https://wallpaperswide.com/download/nature_hd_background-wallpaper-1600x900.jpg",
+  },
+  {
+    id: "3",
+    title: "Image 3",
+    image:
+      "https://images.unsplash.com/photo-1580757468214-c73f7062a5cb?fm=jpg&q=60&w=3000",
+  },
+  {
+    id: "4",
+    title: "Image 4",
+    image:
+      "https://w0.peakpx.com/wallpaper/966/541/HD-wallpaper-park-trees-grass-green-nature-widescreen-16-9-background-1600x900-nature.jpg",
+  },
+  {
+    id: "5",
+    title: "Image 5",
+    image:
+      "https://wallpaperswide.com/download/sunset_312-wallpaper-2048x1152.jpg",
+  },
+];
